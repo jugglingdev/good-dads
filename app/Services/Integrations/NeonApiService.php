332c5a62779cb9410d5e61f2bb33784d58bb5dc5
Daniel Rustad <daniel@pubgene.com>
2025-12-06 01:12:20 -0600

    Updated the fields with missed fields.

--- app/Services/Integrations/NeonApiService.php
+++ app/Services/Integrations/NeonApiService.php
@@ -148,31 +148,26 @@ class NeonApiService
     public function fetchPersonContactInfo(int $personId, bool $useWhereClause): array
     {
         return $this->fetch("persons/{$personId}", [
-            'regions_id',
-            'firstName',
-            'middleName',
-            'lastName',
-            'applicationDate',
-            'address1',
-            'address2',
-            'city',
-            'state',
-            'zip',
-            'employer',
-            'tShirtSize',
-            'homeCellPhone',
-            'workPhone',
-            'otherNumber',
-            'email',
-            'probationParoleCaseWorkerName',
-            'probationParoleCaseWorkerPhone',
-            'contactWithChildren',
-            'contactType',
-            'monthlyChildSupportPayment',
-            'maritalStatus',
-            'ethnicity',
-            'enteredDate',
-            'updatedDate',
+            "regions_id",
+            "enteredDate",
+            "address1",
+            "address2",
+            "city",
+            "state",
+            "zip",
+            "employer",
+            "tShirtSize",
+            "homeCellPhone",
+            "workPhone",
+            "otherNumber",
+            "email",
+            "probationParoleCaseWorkerName",
+            "probationParoleCaseWorkerPhone",
+            "contactWithChildren",
+            "contactType",
+            "monthlyChildSupportPayment",
+            "maritalStatus",
+            "ethnicity"
         ], $useWhereClause);
     }
 
@@ -183,16 +178,30 @@ class NeonApiService
             'lastName',
             // Age to be calculated from dateOfBirth
             'dateOfBirth',
-            'enteredDate',
-            'updatedDate',
         ], $personId, $useWhereClause);
     }
 
     public function fetchPersonDisclosure(int $personId, bool $useWhereClause): array
     {
         return $this->fetch('persons_applications', [
-            'enteredDate',
-            'updatedDate',
+            "persons_id",
+            "division",
+            "divisionOther",
+            "homeCellPhone",
+            "dateOfBirth",
+            "fullAddress",
+            "city",
+            "state",
+            "email",
+            "releaseTo",
+            "releaseToOther",
+            "releaseToOtherAddress",
+            "purposeOfDisclosure",
+            "programName",
+            "purposeOfDisclosureOther",
+            "informationToBeDisclosed",
+            "informationToBeDisclosedOther",
+            "acceptsTextMessage"
         ], $personId, 
         $useWhereClause);
     }
@@ -200,24 +209,94 @@ class NeonApiService
     public function fetchPersonAssessment(int $personId, bool $useWhereClause): array
     {
         return $this->fetch('persons_assessment_worksheet', [
-            'enteredDate',
-            'updatedDate',
+            "persons_id",
+            "fullName",
+            "dateOfBirth",
+            "missouriResident",
+            "childUnder18",
+            "financiallyEligible",
+            "dL",
+            "utilityBill",
+            "payStub",
+            "writtenEmployerStatement",
+            "socialSecurityBenefitsStatement",
+            "selfAttestationOfNoEmploymentOrIncome",
+            "unemploymentCompensation",
+            "other",
+            "hoseholdIncome",
+            "numberOfFamilyMembersInHousehold",
+            "percentageOfFPL"
         ], $personId, $useWhereClause);
     }
 
     public function fetchPersonSurvey(int $personId, bool $useWhereClause): array
     {
         return $this->fetch('persons_introductory_survey', [
-            'enteredDate',
-            'updatedDate',
+            "persons_id",
+            "dateOfBirth",
+            "programName",
+            "reasons",
+            "reasonsOther",
+            "hearAboutUs",
+            "hearAboutUsOther",
+            "expectToGain",
+            "expectToGainOther"
         ], $personId, $useWhereClause);
     }
 
     public function fetchPersonServicePlan(int $personId, bool $useWhereClause): array
     {
         return $this->fetch('persons_service_plan', [
-            'enteredDate',
-            'updatedDate',
+            "persons_id",
+            "programName",
+            "clientNumber",
+            "reviewDates",
+            "serviceAreas",
+            "serviceIdentifiedByTheParticipants",
+            "goals_parentingSkills",
+            "goals_parentingSkillsObj",
+            "goals_parentingSkillsPersonRes",
+            "goals_parentingSkillsTimeline",
+            "goals_parentingSkillsMeasure",
+            "goals_managingStress",
+            "goals_managingStressObj",
+            "goals_managingStressPersonRes",
+            "goals_managingStressTimeline",
+            "goals_managingStressMeasure",
+            "goals_custodyVisitation",
+            "goals_custodyVisitationObj",
+            "goals_custodyVisitationPersonRes",
+            "goals_custodyVisitationMeasure",
+            "goals_educationEmployment",
+            "goals_educationEmploymentObj",
+            "goals_educationEmploymentPersonRes",
+            "goals_educationEmploymentTimeline",
+            "goals_educationEmploymentMeasure",
+            "goals_housingTransportation",
+            "goals_housingTransportationObj",
+            "goals_housingTransportationPersonRes",
+            "goals_housingTransportationTimeline",
+            "goals_housingTransportationMeasure",
+            "goals_childSupportAction",
+            "goals_childSupportActionObj",
+            "goals_childSupportActionPersonRes",
+            "goals_childSupportActionTimeline",
+            "goals_childSupportActionMeasure",
+            "goals_childSupportAwareness",
+            "goals_childSupportAwarenessObj",
+            "goals_childSupportAwarenessPersonRes",
+            "goals_childSupportAwarenessTimeline",
+            "goals_childSupportAwarenessMeasure",
+            "goals_effectiveCoParenting",
+            "goals_effectiveCoParentingObj",
+            "goals_effectiveCoParentingPersonRes",
+            "goals_effectiveCoParentingTimeline",
+            "goals_effectiveCoParentingMeasure",
+            "goals_fatherToFatherMentoring",
+            "goals_fatherToFatherMentoringObj",
+            "goals_fatherToFatherMentoringPersonRes",
+            "goals_fatherToFatherMentoringTimeline",
+            "goals_fatherToFatherMentoringMeasure"
         ], $personId, $useWhereClause);
     }
 
